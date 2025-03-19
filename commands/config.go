37102ce736ee01f3cf3a504650c0a37f107048ad
Grigorii Prudnikov <griprudnikov742@gmail.com>
2025-03-19 19:48:12 +0500

    chore: add usage and --help flag for config command

--- commands/config.go
+++ commands/config.go
@@ -2,31 +2,48 @@ package commands
 
 import (
 	"flag"
+	"fmt"
 	"os"
 )
 
 func Config() error {
 	// TODO: add Usage func
-	var global, get, unset bool
+	// TODO: add --local flag
+	// TODO: add --add flag
+	// TODO: add --get-all flag
+	// TODO: add --unset-all flag
+	var help, global, get, unset bool
 
 	flagSet := flag.NewFlagSet("config", flag.ExitOnError)
 	flagSet.BoolVar(&global, "global", false, "Use global config")
 	flagSet.BoolVar(&get, "get", false, "Get value")
 	flagSet.BoolVar(&unset, "unset", false, "Unset value")
+	flagSet.BoolVar(&help, "help", false, "Show help")
 
 	flagSet.Parse(os.Args[2:])
 	args := flagSet.Args()
 
+	if help {
+		usage()
+		return nil
+	}
 	if get {
-		err := getValue(args, global)
-		return err
+		return getValue(args, global)
 	}
-
 	if unset {
-		err := unsetValue(args, global)
-		return err
+		return unsetValue(args, global)
 	}
 
-	err := setValue(args, global)
-	return err
+	return setValue(args, global)
+}
+
+func usage() {
+	fmt.Println(`usage: notgit config [<options>]
+
+Config file location:
+    --global    use global config file
+    --local     use local config file
+Action:
+    --get       get value
+    --unset     unset value`)
 }
