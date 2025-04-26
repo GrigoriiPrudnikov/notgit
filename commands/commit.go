18152f2bc856470b7f40be95aa4325dac4093d20
Grigorii Prudnikov <griprudnikov742@gmail.com>
2025-04-26 15:11:50 +0500

    chore: add check for repository existence and default author value in commands.Commit

--- commands/commit.go
+++ commands/commit.go
@@ -4,17 +4,32 @@ import (
 	"errors"
 	"flag"
 	"notgit/internal/commit"
+	"notgit/internal/config"
 	"os"
+	"path/filepath"
 )
 
 func Commit() error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	notgitDir := filepath.Join(wd, ".notgit")
+	if _, err := os.Stat(notgitDir); os.IsNotExist(err) {
+		// TODO: add handling for parent directories (fatal: not a git repository (or any of the parent directories): .git)
+		return errors.New("not a git repository")
+	}
+
+	config, err := config.Parse(true)
+
 	var message, author string
 
 	fs := flag.NewFlagSet("commit", flag.ExitOnError)
 
 	fs.StringVar(&message, "m", "", "commit message")
 	fs.StringVar(&message, "message", "", "commit message")
-	fs.StringVar(&author, "author", "", "author")
+	fs.StringVar(&author, "author", config["user"]["name"], "author")
 
 	fs.Parse(os.Args[2:])
 
@@ -23,7 +38,7 @@ func Commit() error {
 		return errors.New("commit creation failed")
 	}
 
-	err := commit.Write()
+	err = commit.Write()
 
 	return err
 }
