b9d8319c5c4b6c0d9a6963032b9a308fc7de648b
Grigorii Prudnikov <griprudnikov742@gmail.com>
2025-08-12 13:48:45 +0500

    feat: add --update flag to "add" command

--- internal/commands/add.go
+++ internal/commands/add.go
@@ -21,7 +21,7 @@ func Add() error {
 		return errors.New("not a notgit repository")
 	}
 
-	var all, force bool
+	var all, force, update bool
 
 	fs := flag.NewFlagSet("add", flag.ExitOnError)
 
@@ -31,17 +31,13 @@ func Add() error {
 	fs.BoolVar(&force, "force", false, "force")
 	fs.BoolVar(&force, "f", false, "force")
 
-	// fs.BoolVar(&update, "update", false, "update")
-	// fs.BoolVar(&update, "u", false, "update")
+	fs.BoolVar(&update, "update", false, "update")
+	fs.BoolVar(&update, "u", false, "update")
 
 	fs.Parse(os.Args[2:])
 	args := fs.Args()
 
-	if len(args) == 0 {
-		return nil
-	}
-
-	if slices.Contains(args, ".") {
+	if slices.Contains(args, ".") || update {
 		all = true
 	}
 
@@ -50,58 +46,25 @@ func Add() error {
 		return err
 	}
 
-	for _, path := range args {
-		info, err := os.Stat(path)
+	if update {
+		err := addPathToIndex(".", true, &index)
 		if err != nil {
 			return err
 		}
-
-		if info.IsDir() {
-			if utils.Ignored(path) {
-				continue
-			}
-
-			paths := []string{}
-			err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
-				if err != nil {
-					return err
-				}
-				if info.IsDir() {
-					return nil
-				}
-				paths = append(paths, path)
-				return nil
-			})
-			if err != nil {
-				return err
-			}
-
-			for _, path := range paths {
-				if utils.Ignored(path) {
-					continue
-				}
-
-				// todo: here add check for --update flag
-
-				b, err := blob.NewBlob(path)
-				if err != nil {
-					return err
-				}
-				index[path] = b.Hash()
-			}
-			continue
-		}
-
-		b, err := blob.NewBlob(path)
-		if err != nil {
-			return err
-		}
-
-		index[path] = b.Hash()
 	}
 
-	// here add update logic
 	if all {
+		args = []string{"."}
+	}
+
+	for _, path := range args {
+		err := addPathToIndex(path, update, &index)
+		if err != nil {
+			return err
+		}
+	}
+
+	if all || update {
 		for path := range index {
 			_, err := os.Stat(path)
 
@@ -112,4 +75,61 @@ func Add() error {
 	}
 
 	return indexfile.Write(index)
+}
+
+func addPathToIndex(path string, update bool, index *map[string]string) error {
+	info, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	if info.IsDir() {
+		if utils.Ignored(path) {
+			return nil
+		}
+
+		paths := []string{}
+		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+			paths = append(paths, path)
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+
+		for _, path := range paths {
+			if utils.Ignored(path) {
+				continue
+			}
+
+			if update {
+				// check if path is already in index
+				if _, ok := (*index)[path]; !ok {
+					continue
+				}
+			}
+
+			b, err := blob.NewBlob(path)
+			if err != nil {
+				return err
+			}
+			(*index)[path] = b.Hash()
+		}
+		return nil
+	}
+
+	b, err := blob.NewBlob(path)
+	if err != nil {
+		return err
+	}
+
+	(*index)[path] = b.Hash()
+
+	return nil
 }
